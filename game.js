b01251cd5c06de1952a966f529ba033881aca19a
Mike Morici <himedlooff@yahoo.com>
2015-09-05 23:27:46 -0400

    Add temp event listener

--- game.js
+++ game.js
@@ -3,3 +3,7 @@ var canvas = canvasEl.getContext('2d');
 
 canvas.fillStyle = 'green';
 canvas.fillRect(10, 10, 100, 100);
+
+canvasEl.addEventListener('keydown', function() {
+    canvas.fillStyle = 'red';           canvas.fillRect(10, 10, 100, 100);
+});
